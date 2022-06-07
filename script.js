754d55601e15fe51159841a423fbf4838f1595d5
Marcos <marcosmaiafidelis777@outlook.com>
2022-06-07 15:45:57 -0300

    value

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.busca').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let input = document.querySelector('#searchInput').value;
+
+    console.log(input)
+});
